b4a2050e3ff93ef28b51498c56abaa85987b48a3
iamhb <its.hari.programmer@gmail.com>
2020-05-18 14:53:04 +0530

    development branch first commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 let express = require('express');
 let app = express();
+let PORT = 3000;
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let stream = require('./ws/stream');
 let path = require('path');
-let favicon = require('serve-favicon')
+let favicon = require('serve-favicon');
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 
 io.of('/stream').on('connection', stream);
 
-server.listen(3000);
+server.listen(PORT, function () {
+    console.log(`Server started at ${PORT}`);
+});
