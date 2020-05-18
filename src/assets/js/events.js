a83bdd8f0626db2e5a6f22b301cf5831416558b6
iamhb <its.hari.programmer@gmail.com>
2020-05-18 15:26:37 +0530

    Room link visible

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -1,28 +1,28 @@
 import helpers from './helpers.js';
 
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
     //When the chat icon is clicked
-    document.querySelector('#toggle-chat-pane').addEventListener('click', (e)=>{
+    document.querySelector('#toggle-chat-pane').addEventListener('click', (e) => {
         let chatElem = document.querySelector('#chat-pane');
         let mainSecElem = document.querySelector('#main-section');
 
-        if(chatElem.classList.contains('chat-opened')){
+        if (chatElem.classList.contains('chat-opened')) {
             chatElem.setAttribute('hidden', true);
             mainSecElem.classList.remove('col-md-9');
             mainSecElem.classList.add('col-md-12');
             chatElem.classList.remove('chat-opened');
         }
 
-        else{
+        else {
             chatElem.attributes.removeNamedItem('hidden');
             mainSecElem.classList.remove('col-md-12');
             mainSecElem.classList.add('col-md-9');
-            chatElem.classList.add('chat-opened');          
+            chatElem.classList.add('chat-opened');
         }
 
         //remove the 'New' badge on chat icon (if any) once chat is opened.
-        setTimeout(()=>{
-            if(document.querySelector('#chat-pane').classList.contains('chat-opened')){
+        setTimeout(() => {
+            if (document.querySelector('#chat-pane').classList.contains('chat-opened')) {
                 helpers.toggleChatNotificationBadge();
             }
         }, 300);
@@ -30,33 +30,33 @@ window.addEventListener('load', ()=>{
 
 
     //When the video frame is clicked. This will enable picture-in-picture
-    document.getElementById('local').addEventListener('click', ()=>{
+    document.getElementById('local').addEventListener('click', () => {
         if (!document.pictureInPictureElement) {
             document.getElementById('local').requestPictureInPicture()
-            .catch(error => {
-                // Video failed to enter Picture-in-Picture mode.
-                console.error(error);
-            });
-        } 
-          
+                .catch(error => {
+                    // Video failed to enter Picture-in-Picture mode.
+                    console.error(error);
+                });
+        }
+
         else {
             document.exitPictureInPicture()
-            .catch(error => {
-                // Video failed to leave Picture-in-Picture mode.
-                console.error(error);
-            });
+                .catch(error => {
+                    // Video failed to leave Picture-in-Picture mode.
+                    console.error(error);
+                });
         }
     });
 
 
     //When the 'Create room" is button is clicked
-    document.getElementById('create-room').addEventListener('click', (e)=>{
+    document.getElementById('create-room').addEventListener('click', (e) => {
         e.preventDefault();
 
         let roomName = document.querySelector('#room-name').value;
         let yourName = document.querySelector('#your-name').value;
 
-        if(roomName && yourName){
+        if (roomName && yourName) {
             //remove error message, if any
             document.querySelector('#err-msg').innerHTML = "";
 
@@ -67,27 +67,30 @@ window.addEventListener('load', ()=>{
             let roomLink = `${location.origin}?room=${roomName.trim().replace(' ', '_')}_${helpers.generateRandomString()}`;
 
             //show message with link to room
-            document.querySelector('#room-created').innerHTML = `Room successfully created. Click <a href='${roomLink}'>here</a> to enter room. 
-                Share the room link with your partners.`;
+            document.querySelector('#room-created').innerHTML = `Room successfully created. Click <a href='${roomLink}'>here</a> to enter room. `
+            // Share the room link with your partners.`;
+
+            // show room link 
+            document.querySelector('#show-room-link').innerHTML = 'Share this link to join directly to your room: ' + roomLink;
 
             //empty the values
             document.querySelector('#room-name').value = '';
             document.querySelector('#your-name').value = '';
         }
 
-        else{
+        else {
             document.querySelector('#err-msg').innerHTML = "All fields are required";
         }
     });
 
 
     //When the 'Enter room' button is clicked.
-    document.getElementById('enter-room').addEventListener('click', (e)=>{
+    document.getElementById('enter-room').addEventListener('click', (e) => {
         e.preventDefault();
 
         let name = document.querySelector('#username').value;
 
-        if(name){
+        if (name) {
             //remove error message, if any
             document.querySelector('#err-msg-username').innerHTML = "";
 
@@ -98,24 +101,24 @@ window.addEventListener('load', ()=>{
             location.reload();
         }
 
-        else{
+        else {
             document.querySelector('#err-msg-username').innerHTML = "Please input your name";
         }
     });
 
 
-    document.addEventListener('click', (e)=>{
-        if(e.target && e.target.classList.contains('expand-remote-video')){
+    document.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('expand-remote-video')) {
             helpers.maximiseStream(e);
         }
 
-        else if(e.target && e.target.classList.contains('mute-remote-mic')){
+        else if (e.target && e.target.classList.contains('mute-remote-mic')) {
             helpers.singleStreamToggleMute(e);
         }
     });
 
 
-    document.getElementById('closeModal').addEventListener('click', ()=>{
+    document.getElementById('closeModal').addEventListener('click', () => {
         helpers.toggleModal('recording-options-modal', false);
     });
 })
